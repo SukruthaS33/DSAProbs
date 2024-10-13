810d97eb9bec004149335be1cf55351c03ef1d25
Sukrutha Shamsundar <sukrutha2024@gmail.com>
2024-10-13 22:37:22 +0530

    Update HackerRankAsks.java

--- gpt/HackerRankQuestions/HackerRankAsks.java
+++ gpt/HackerRankQuestions/HackerRankAsks.java
@@ -2,43 +2,28 @@ package com.sukrutha.gpt.questions;
 
 public class HackerRankAsks {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
+        // Reverse an array
+        int[] array = {1, 2, 3, 4, 5, 78, 23, 32, 32, 64, 6};
+        
+        // Two-pointer approach
+        int left = 0;                  // Start pointer
+        int right = array.length - 1; // End pointer
+        
+        while (left < right) {
+            // Swap the elements
+            int temp = array[left];
+            array[left] = array[right];
+            array[right] = temp;
+            
+            // Move the pointers
+            left++;
+            right--;
+        }
 
-// reverse an array
-		int[] array = { 1, 2, 3, 4, 5,78,23,32,32,64,6 };
-		int arrLength = array.length;
-		int lengthEven = arrLength % 2;// remainder is returned
-		int skipAt = -1;
-		int j = arrLength - 1;
-		boolean isLengthEven = false;
-		if (lengthEven == 0) {
-			System.out.println("inside if");
-			isLengthEven = true;
-		} else {
-			System.out.println("inside else");
-			skipAt = arrLength / 2;
-		}
-
-		for (int i = 0; i < array.length; i++) {
-			if (i != skipAt && i != array.length / 2) {
-
-				int temp = array[i];
-				array[i] = array[j];
-				array[j] = temp;
-				System.out.println(array[i]);
-				System.out.println(array[j]);
-				System.out.println("arrange done");
-				j--;
-
-			} else {
-				System.out.println("inside else");
-				break;
-			}
-		}
-		System.out.println("===================");
-		for (int i = 0; i < array.length; i++) {
-			System.out.println(array[i]);
-		}
-	}
-
+        // Print the reversed array
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+    }
 }
