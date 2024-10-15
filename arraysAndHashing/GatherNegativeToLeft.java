fb188d438184123aab44afdcfb2b47d5c78d6909
Sukrutha Shamsundar <sukrutha2024@gmail.com>
2024-10-15 22:49:52 +0530

    Update GatherNegativeToLeft.java

--- arraysAndHashing/GatherNegativeToLeft.java
+++ arraysAndHashing/GatherNegativeToLeft.java
@@ -2,8 +2,9 @@ package com.sukrutha.gpt.questions;
 
 public class GatherNegativeToLeft {
 
-	static int[] array = { 1, -2, 1, 4, 5, -5, -6, -1, -4, 0 };// order doesn't matter
+	//static int[] array = { 1, -2, 1, 4, 5, -5, -6, -1, -4, 0 };// order doesn't matter
 
+	static int[] array = {0,0,0};//edge case
 	public static void main(String[] args) {
 		GatherNegativeToLeft(array);
 		for (int i = 0; i < array.length; i++) {
@@ -16,7 +17,7 @@ public class GatherNegativeToLeft {
 		int temp;
 		for (int i = 1; i < array.length; i++) {
 			// start from 1 because it's the right way to compare
-			if (array[i] < 0 && array[i - 1] > 0) {// if the current element is less than 0 and the previous element is
+			if (array[i] < 0 && array[i - 1] >=0) {// if the current element is less than 0 and the previous element is
 				// greater than 0 then
 				temp = array[i];//temp = -2
 				array[i] = array[lastNegIndex + 1];
