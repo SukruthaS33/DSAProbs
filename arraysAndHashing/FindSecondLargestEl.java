decda0e7d04bfc40922876369591d467a96a533c
Sukrutha Shamsundar <sukrutha2024@gmail.com>
2024-10-23 10:12:53 +0530

    Update FindSecondLargestEl.java

--- arraysAndHashing/FindSecondLargestEl.java
+++ arraysAndHashing/FindSecondLargestEl.java
@@ -2,7 +2,7 @@ package com.sukrutha.gpt.questions;
 
 public class FindSecondLargestEl {
 
-	static int[] array = { 3, 7, 1, 8, 9, 4, 6 };
+	static int[] array = { 4, 7, 7, 7, 6, 5 };
 
 	public static void main(String[] args) {
 		findSecondLargest();
@@ -11,14 +11,20 @@ public class FindSecondLargestEl {
 
 	public static int findSecondLargest() {
 
-		int firstLargest = array[1];
-		int secondLargest = array[0];
+		int firstLargest = -1;
+		int secondLargest =-1;;
 
 		for (int i = 1; i < array.length; i++) {
-			if (array[i] > firstLargest) {
-				secondLargest = firstLargest;
-				firstLargest = array[i];
+			if (array[i] > firstLargest) {//check if current element is greater than firstlargest 
+
+				secondLargest = firstLargest;//if true assign the secondLargest to previous firstlargest because current elemnt is greater than largest
+				firstLargest = array[i];//firstlargest will array[i]
 			}
+
+			else if (array[i] > secondLargest && array[i]<firstLargest) {//can also be written as array[i]!=firstlargest //but if array[i] is greater than secondLargest and also not equal to firstLargest
+				secondLargest = array[i];
+			}
+
 		}
 		return secondLargest;
 	}
