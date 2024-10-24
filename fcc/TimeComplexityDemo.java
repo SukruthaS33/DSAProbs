53aed9267f79c6eb026db6b2b091346ae1e7d04c
Sukrutha Shamsundar <sukrutha2024@gmail.com>
2024-10-24 11:01:14 +0530

    Create TimeComplexityDemo.java

---
+++ fcc/TimeComplexityDemo.java
@@ -0,0 +1,32 @@
+package com.freecodecampdsa.first;
+
+public class TimeComplexityDemo {
+
+	public static void main(String[] args) {
+
+		TimeComplexityDemo demo1 = new TimeComplexityDemo();
+		double now = System.nanoTime();
+
+		demo1.findSum(1000);
+		System.out.println(System.nanoTime() - now +"ms");
+		demo1.findSum2(4);
+		System.out.println(System.nanoTime() - now+"ms");
+
+	}
+
+	public int findSum(int n) {
+
+		return n * (n + 1) / 2;
+
+	}
+
+	public int findSum2(int n) {
+
+		int sum = 0;
+		for (int i = 0; i <= n; i++) {
+			sum = sum + i;
+		}
+		return sum;
+	}
+
+}
